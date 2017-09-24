883a11d7d5f6f41cb6a07f880c77273378ed7cc0
suchaone <invisible.chinchilla@gmail.com>
2017-09-24 17:57:46 -0400

    sad moon tribute

--- js/script.js
+++ js/script.js
@@ -16,4 +16,18 @@ $(document).ready(function() {
       }, 5000);
     },5000);
   });
+
+  function countMemories () {
+    $.ajax({
+      url: "https://spreadsheets.google.com/feeds/list/1Nsth47pK8zYoMxtf_Q1PGQBYbegyhQxZCfg-AEMbJ1U/od6/public/values?alt=json-in-script&callback=DELETEME",
+      done: function(data) {
+        var x = data.responseText;
+        x = x.replace("DELETEME(", "");
+        x = x.replace("// API callback", "");
+        x = x.substring(0, x.length - 2);
+        console.log($.parseJSON(x).feed.entry);
+      }
+    });
+  }
+
 });
